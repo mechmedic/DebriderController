6c2a1778070fbd99e3307e7ba3914122e4dbd7a9
Veysi ADIN <veysi.adin@kist.re.kr>
2020-08-06 15:28:43 +0900

    Veysi ADIN DESKTOP TEST1

---
+++ motorthread.h
@@ -0,0 +1,60 @@
+#ifndef MOTORTHREAD_H
+#define MOTORTHREAD_H
+
+#include <QThread>
+#include "epos4_can.h"
+#include <QElapsedTimer>
+#include <m_defines.h>
+#include <wiringPi.h>
+#include <serial_com.h>
+class motorThread : public QThread
+{
+    Q_OBJECT
+
+public:
+
+    motorThread() {
+        m_currState = DEBRIDER_STATE_INIT;
+        m_prevState = DEBRIDER_STATE_INIT;
+        m_TargetVel = 0;
+        m_Oscillate = 0;
+        m_TargetPos = 0;        
+        m_CloseBlade=0;
+        m_emergency=0;
+        m_NewTargetVel=0;
+        btn_MAXRPM_LastState=0;
+        btn_ChangeDirLastState=0;
+        m_closeBladeWindow=0;
+        btn_MAXRPM_GUI=0;
+        btn_ChangeDir_GUI=0;
+    }
+    virtual void run();
+    long m_NewTargetVel;
+    int m_TargetVel;
+    int m_Oscillate;
+    bool m_running;
+    int m_CloseBlade;            // VysADN CloseBlade function parameters
+    int m_TargetPos;            
+    int m_emergency;
+    int m_closeBladeWindow;
+    int btn_MAXRPM_GUI;
+    int btn_ChangeDir_GUI;
+private:
+    // CKim - Motor class
+    int m_currState, m_prevState;
+    MaxonMotor m_Motor;
+    int m_RightPedalDown;
+    int m_LeftPedalDown;
+    int btn_ChangeDirLastState;
+    int btn_MAXRPM_LastState;
+    serial_com arduino;
+    QElapsedTimer timer;
+    bool WD_State=true;
+signals:
+    void UpdateGUI(int state);
+};
+
+
+
+
+#endif // MOTORTHREAD_H
---
+++ serial_com.h
@@ -0,0 +1,56 @@
+#ifndef SERIAL_COM_H
+#define SERIAL_COM_H
+
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <qserialportinfo.h>
+#include <QThread>
+
+class serial_com : public QThread
+{   
+private:
+
+    virtual void run();
+    bool s_errorFlag = false;               // Error flag for serial communication errors.
+
+    // ### VysADN 2 byte serial communication protocol with arduino nano every variables for reciever (Raspberry Pi3b+) side.  ### (Details about these variables is explained in readData() function)
+    unsigned char FirstByte, SecondByte;
+    int NumRead=0 ,numReadTotal=0;
+    char get_Pack[50]={};
+
+    // ### Arduino Nano Every product id and vendor id , universal for all Arduino Nano Every ###
+    static const quint16 ArduinoNanoEvery_vendor_id =9025;
+    static const quint16 ArduinoNanoEvery_product_id =88;
+
+public:
+    serial_com(){
+        btn_MAXRPM=0;
+        btn_CloseBlade=0;
+        btn_ChangeDirection=0;
+        Get_Analog_Pedal_Val=0;
+    }
+
+    QSerialPort serial;
+
+    int btn_MAXRPM;
+
+    int btn_CloseBlade;
+
+    int btn_ChangeDirection;
+
+    int Get_Analog_Pedal_Val;
+
+    void SerialSetError(bool s);
+
+    bool SerialGetError();
+
+    void ClearPort();
+
+    void readData();
+
+    void openSerialPort();
+
+    void closeSerialPort();
+};
+
+#endif // SERIAL_COM_H
